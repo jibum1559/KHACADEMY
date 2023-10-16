f17e7555f524357a397de974141d7a2118efd5b4
jibum1559 <143372311+jibum1559@users.noreply.github.com>
2023-10-16 16:05:45 +0900

    dir add

---
+++ academy_java/chap21_2차원 배열/src/com/kh/arrayMain/ArrayMain.java
@@ -0,0 +1,26 @@
+package com.kh.arrayMain;
+
+import com.kh.ArrayExam.ArrayExam;
+
+public class ArrayMain {
+	public static void main(String[] args) {
+		ArrayExam practice = new ArrayExam();
+		//practice.practice1();
+		//practice.practice2();
+		practice.practice4();
+	}
+}
+
+
+
+
+/*
+==1분단==
+강건강 남나나 
+도대담 류라라 
+문미미 박보배 
+==2분단==
+송성실 윤예의 
+진재주 차천축 
+피풍표 홍하하 
+*/
